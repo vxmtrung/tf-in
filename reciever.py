580be111522c1e4d9cdf6e02971453a715f264f3
vxmtrung <vuxuanmaitrung1821@gmail.com>
2023-11-24 20:21:09 +0700

    change transfer 4

--- reciever.py
+++ reciever.py
@@ -1,41 +1,29 @@
-# This file will be used for recieving files over socket connection.
-import os
-import socket
-import time
-
-host = input("Host Name: ")
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Trying to connect to socket.
-try:
-    sock.connect((host, 22222))
-    print("Connected Successfully")
-except:
-    print("Unable to connect")
-    exit(0)
-
-# Send file details.
-file_name = sock.recv(100).decode()
-file_size = sock.recv(100).decode()
-
-# Opening and reading file.
-with open("./rec/" + file_name, "wb") as file:
-    c = 0
-    # Starting the time capture.
-    start_time = time.time()
-
-    # Running the loop while file is recieved.
-    while c <= int(file_size):
-        data = sock.recv(1024)
-        if not (data):
-            break
-        file.write(data)
-        c += len(data)
-
-    # Ending the time capture.
-    end_time = time.time()
-
-print("File transfer Complete.Total time: ", end_time - start_time)
-
-# Closing the socket.
-sock.close()
+import socket,time,os
+ 
+# Server configuration
+host = socket.gethostbyname(socket.gethostname())        # Listen on this host
+port = 4420             # Main port to listen on
+ 
+d = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+d.bind((host, port))
+d.listen(1)
+s,a = d.accept()
+ 
+f = s.recv(1024)
+z = f.decode()
+if z.endswith("=EOFX="):
+    x = str(z).split()
+    e = str(x[1])
+    n = str(" ".join(x[2:-1]))
+    print("-- Receive file: " + n + " (" + e + ")")
+    g = open(n, 'wb')
+    while True:
+        l = s.recv(1024)
+        try: 
+            if l.decode().endswith('=EOFX=') == True: break
+        except: pass
+        g.write(l)
+    g.close()
+    if e == str(os.path.getsize(n)): print(">> Size verified.")
+    else: print("!! Size mismatch.")
+    s.close()
--- sender.py
+++ sender.py
@@ -1,42 +1,29 @@
-# This file is used for sending the file over socket
-import os
-import socket
-import time
-
-# Creating a socket.
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((socket.gethostname(), 22222))
-sock.listen(5)
-print("Host Name: ", sock.getsockname())
-
-# Accepting the connection.
-client, addr = sock.accept()
-
-# Getting file details.
-file_name = input("File Name:")
-file_size = os.path.getsize(file_name)
-
-# Sending file_name and detail.
-client.send(file_name.encode())
-client.send(str(file_size).encode())
-
-# Opening file and sending data.
-with open(file_name, "rb") as file:
-    c = 0
-    # Starting the time capture.
-    start_time = time.time()
-
-    # Running loop while c != file_size.
-    while c <= file_size:
-        data = file.read(1024)
-        if not (data):
-            break
-        client.sendall(data)
-        c += len(data)
-
-    # Ending the time capture.
-    end_time = time.time()
-
-print("File Transfer Complete.Total time: ", end_time - start_time)
-# Cloasing the socket.
-sock.close()
+import socket,time,os,sys
+PORT = 4420
+if len(sys.argv) > 2:
+    host = str(sys.argv[1])
+    file = str(' '.join(sys.argv[2:]))
+else: 
+    print("> Usage: filesend.py host file")
+    sys.exit()
+ 
+# Configure socket connection
+z = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+z.connect((host, PORT))
+ 
+flen = str(os.path.getsize(file))
+fstr = 'FILE: ' + flen + ' ' + os.path.basename(file) + ' =EOFX='
+print("- Send file: " + file + " (" + flen + ")")
+z.send(fstr.encode())
+time.sleep(1)
+try:
+    with open(file, 'rb') as f:
+        fileData = f.read()
+        # Begin sending file
+        z.sendall(fileData)
+        time.sleep(4)
+        z.send('=EOFX='.encode())
+    f.close()
+    print('>> Transfer: ' + file + ' complete.\n')
+except:
+    print('> Error sending file: ' + file + '.\n')
